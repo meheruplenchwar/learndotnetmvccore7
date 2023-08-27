e89b2b3413271191e0b30d6c693161d775a4e340
Meherkumar Uplenchwar <v-muplenchwa@microsoft.com>
2023-08-27 19:22:52 +0530

    Deleted_unwanted_files_to_start_from_scratch

--- Proj_Learn_ASP_Net_Core_MVC_7/Proj_Learn_ASP_Net_Core_MVC_7/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proj_Learn_ASP_Net_Core_MVC_7.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
