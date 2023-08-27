351a74084f6e75e4cb7b1de76915e1d3d8d8b22e
Meherkumar Uplenchwar <v-muplenchwa@microsoft.com>
2023-08-27 18:33:09 +0530

    Create_Project

---
+++ Proj_Learn_ASP_Net_Core_MVC_7/Proj_Learn_ASP_Net_Core_MVC_7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proj_Learn_ASP_Net_Core_MVC_7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
